b6f16654eb6ee7c2d72b91bd6ffa2a7e53bd39e5
takuma39 <takuma.sato.job@gmail.com>
2022-09-06 13:41:28 +0900

    Start 環境構築　+　bg追加

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,26 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <!-- <router-view/> -->
 </template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'AppView',
+  components: {
+  },
+  data(){
+    return {
+    }
+  },
+  methods: {},
+  mounted() {
+
+  }
+}
+
+</script>
 
 <style lang="scss">
 #app {
@@ -24,7 +42,12 @@ nav {
 
     &.router-link-exact-active {
       color: #42b983;
+      color: #0080ff;
     }
   }
+}
+
+body{
+  z-index: -2;
 }
 </style>
