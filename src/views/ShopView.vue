3179ae5840188e4cf2a98f1aa77dd12602431af5
takuma39 <takuma.sato.job@gmail.com>
2022-09-08 11:18:22 +0900

    Add ShopPage

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,19 +1,72 @@
 <template>
 <Header/>
   <div class="shop">
-    <h1>This is an shop page</h1>
+    <div class="shop__header">
+      <PageTitle title="Shop"/>
+    </div>
+    <div class="shop__inner">
+      <p>商品お取り扱い店舗はこちら</p>
+      <ul class="shop__list">
+        <li>
+          <img alt="logo" src="@/assets/logo.png">
+          <p>店舗A</p>
+        </li>
+        <li>
+          <img alt="logo" src="@/assets/logo.png">
+          <p>店舗B</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from '@/layouts/HeaderLayout.vue';
+import PageTitle from '@/components/PageTitle.vue';
 export default {
   components: {
-    Header
+    Header,
+    PageTitle
   },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.shop{
+  &__header{
+    margin-top: 100px;
+  }
+
+  &__inner{
+    width:100%;
+    max-width: 960px;
+    margin: 0 auto;
+    background-color: rgba($color: #fff, $alpha: 0.5);
+    border-radius: 10%;
+    padding: 60px 0px;
+    margin-top: 30px;
+
+    p{
+      text-align: center;
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+
+  &__list{
+    margin-top: 60px;
+    display: flex;
+    justify-content: center;
+
+    li{
+      width: 200px;
+      margin: 0 60px;
+
+      p{
+        margin-top: 20px;
+      }
+    }
+  }
+}
 
 </style>
