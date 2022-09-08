4bfccd841cf15b17c48ae41b00ce211a2296c840
takuma39 <takuma.sato.job@gmail.com>
2022-09-08 11:08:24 +0900

    Add PageTitleComponent

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1><span>{{ title }}</span></h1>
+</template>
+
+<script>
+export default {
+  name: 'PageTitle',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  h1{
+    text-align: center;
+    font-size: 50px;
+    font-weight: bold;
+    color:#0080ff;
+
+    span{
+      border-bottom:5px solid  #0080ff;
+    }
+  }
+</style>
