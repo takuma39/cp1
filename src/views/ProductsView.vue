8c57cb4b9e58f9fa0489006869f177849e2cc75f
takuma39 <takuma.sato.job@gmail.com>
2022-09-12 11:45:48 +0900

    Add Products PC画面

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,16 +1,406 @@
 <template>
   <div class="products">
-    <h1>This is an products page</h1>
+    <div class="shop__header">
+      <PageTitle title="Products"/>
+    </div>
+    <div class="products__inner">
+      <ul class="products__list">
+        <li class="products__item">
+          <h3>
+            <span data-type="name" data-color="blue">BLUE</span>
+            <span data-type="text">ボリュームのでにくい髪を、ふんわりと弾力のある艶髪へ</span>
+          </h3>
+          <div class="products__flex">
+          <div class="products__image">
+            <img src="@/assets/images/bottles/bottles_blue.png" alt="blue">
+          </div>
+          <div class="products__textArea">
+
+            <div class="products__features">
+              <h4>こんな髪質の方へ</h4>
+              <ul>
+                <li>
+                  <p>細毛、 軟毛</p>
+                </li>
+                <li>
+                  <p>ふんわり感、弾力を求める方</p>
+                </li>
+              </ul>
+            </div>
+
+            <div class="products__priceList">
+              <div class="products__price" data-type="shampoo">
+                <h5>shampoo</h5>
+                <ul>
+                  <li>
+                    <p>250mL</p>
+                    <p>2,970円</p>
+                  </li>
+                  <li>
+                    <p>500mL</p>
+                    <p>4,950円</p>
+                  </li>
+                  <li>
+                    <p>1000mL</p>
+                    <p>6,000円（詰替）</p>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="products__price" data-type="treatment">
+                <h5>treatment</h5>
+                <ul>
+                  <li>
+                    <p>200g</p>
+                    <p>3,190円</p>
+                  </li>
+                  <li>
+                    <p>500g</p>
+                    <p>6,600円</p>
+                  </li>
+                  <li>
+                    <p>1000g</p>
+                    <p>8,140円（詰替）</p>
+                  </li>
+                </ul>
+              </div>
+            </div>            
+
+          </div>
+          </div>
+        </li>
+        <li class="products__item">
+          <h3>
+            <span data-type="name" data-color="white">WHITE</span>
+            <span data-type="text">からまりやすい髪を、サラサラでなめらかな艶髪へ</span>
+          </h3>
+          <div class="products__flex">
+          <div class="products__image">
+            <img src="@/assets/images/bottles/bottles_white.png" alt="blue">
+          </div>
+          <div class="products__textArea">
+
+            <div class="products__features">
+              <h4>こんな髪質の方へ</h4>
+              <ul>
+                <li>
+                  <p>細毛、軟毛～普通毛</p>
+                </li>
+                <li>
+                  <p>サラサラ感を求める方</p>
+                </li>
+              </ul>
+            </div>
+
+            <div class="products__priceList">
+              <div class="products__price" data-type="shampoo">
+                <h5>shampoo</h5>
+                <ul>
+                  <li>
+                    <p>250mL</p>
+                    <p>2,970円</p>
+                  </li>
+                  <li>
+                    <p>500mL</p>
+                    <p>4,950円</p>
+                  </li>
+                  <li>
+                    <p>1000mL</p>
+                    <p>6,000円（詰替）</p>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="products__price" data-type="treatment">
+                <h5>treatment</h5>
+                <ul>
+                  <li>
+                    <p>200g</p>
+                    <p>3,190円</p>
+                  </li>
+                  <li>
+                    <p>500g</p>
+                    <p>6,600円</p>
+                  </li>
+                  <li>
+                    <p>1000g</p>
+                    <p>8,140円（詰替）</p>
+                  </li>
+                </ul>
+              </div>
+            </div>            
+
+          </div>
+          </div>
+        </li>
+        <li class="products__item">
+          <h3>
+            <span data-type="name" data-color="green">GREEN</span>
+            <span data-type="text">ダメージで乾燥した髪を、しっとりとうるおいのある艶髪へ</span>
+          </h3>
+          <div class="products__flex">
+          <div class="products__image">
+            <img src="@/assets/images/bottles/bottles_green.png" alt="blue">
+          </div>
+          <div class="products__textArea">
+
+            <div class="products__features">
+              <h4>こんな髪質の方へ</h4>
+              <ul>
+                <li>
+                  <p>太毛、硬毛</p>
+                </li>
+                <li>
+                  <p>しっとり感、まとまり感を求める方</p>
+                </li>
+              </ul>
+            </div>
+
+            <div class="products__priceList">
+              <div class="products__price" data-type="shampoo">
+                <h5>shampoo</h5>
+                <ul>
+                  <li>
+                    <p>250mL</p>
+                    <p>2,970円</p>
+                  </li>
+                  <li>
+                    <p>500mL</p>
+                    <p>4,950円</p>
+                  </li>
+                  <li>
+                    <p>1000mL</p>
+                    <p>6,000円（詰替）</p>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="products__price" data-type="treatment">
+                <h5>treatment</h5>
+                <ul>
+                  <li>
+                    <p>200g</p>
+                    <p>3,190円</p>
+                  </li>
+                  <li>
+                    <p>500g</p>
+                    <p>6,600円</p>
+                  </li>
+                  <li>
+                    <p>1000g</p>
+                    <p>8,140円（詰替）</p>
+                  </li>
+                </ul>
+              </div>
+            </div>            
+
+          </div>
+          </div>
+        </li>
+        <li class="products__item">
+          <h3>
+            <span data-type="name" data-color="orange">ORANGE</span>
+            <span data-type="text">ダメージでかたくなった髪を、やわらかくなめらかな艶髪へ</span>
+          </h3>
+          <div class="products__flex">
+          <div class="products__image">
+            <img src="@/assets/images/bottles/bottles_orange.png" alt="blue">
+          </div>
+          <div class="products__textArea">
+
+            <div class="products__features">
+              <h4>こんな髪質の方へ</h4>
+              <ul>
+                <li>
+                  <p>普通毛～太毛、硬毛</p>
+                </li>
+                <li>
+                  <p>やわらかさを求める方</p>
+                </li>
+              </ul>
+            </div>
+
+            <div class="products__priceList">
+              <div class="products__price" data-type="shampoo">
+                <h5>shampoo</h5>
+                <ul>
+                  <li>
+                    <p>250mL</p>
+                    <p>2,970円</p>
+                  </li>
+                  <li>
+                    <p>500mL</p>
+                    <p>4,950円</p>
+                  </li>
+                  <li>
+                    <p>1000mL</p>
+                    <p>6,000円（詰替）</p>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="products__price" data-type="treatment">
+                <h5>treatment</h5>
+                <ul>
+                  <li>
+                    <p>200g</p>
+                    <p>3,190円</p>
+                  </li>
+                  <li>
+                    <p>500g</p>
+                    <p>6,600円</p>
+                  </li>
+                  <li>
+                    <p>1000g</p>
+                    <p>8,140円（詰替）</p>
+                  </li>
+                </ul>
+              </div>
+            </div>            
+
+          </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import PageTitle from '@/components/PageTitle.vue';
 export default {
   components: {
+    PageTitle,
   },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "@/assets/sass/app2.scss";
+.products{
+  padding: 100px 0 50px;
 
+  &__inner{
+    width: 90%;
+    max-width: 1000px;
+    margin:20px auto 0 auto;
+    background-color: rgba($color: #FFF, $alpha: 0.8);
+  }
+
+  &__list{
+  }
+
+  &__item{
+    padding: 20px;
+
+    &:nth-child(n +2){
+      margin-top: 20px;
+    }
+
+    h3{
+      font-weight: bold;
+      display: flex;
+      align-items: center;
+
+      span[data-type="name"]{
+        @include rem(40);
+      }
+      span[data-color="blue"]{
+        color: #99c8e1;
+      }
+      span[data-color="white"]{
+        color: #eae3bf;
+      }
+      span[data-color="green"]{
+        color: #B1D7B4;
+      }
+      span[data-color="orange"]{
+        color: #ebc29c;
+      }
+      span[data-type="text"]{
+        margin-left: 30px;
+        @include rem(20);
+      }
+    }
+  }
+
+  &__flex{
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
+
+  &__image{
+    width: 35%;
+    max-width: 3500px;
+  }
+
+  &__textArea{
+    width: 60%;
+  }
+
+  &__features{
+    h4{
+      @include rem(20);
+      font-weight: bold;
+    }
+
+    ul{
+      margin-top: 10px;
+      border-top: dashed 1px #717071;
+
+
+      li{
+        padding: 10px;
+        border-bottom: dashed 1px #717071;
+
+        p{
+          @include rem(16);
+          font-weight: bold;
+        }
+      }
+    }
+  }
+
+  &__priceList{
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-between;
+
+    h5{
+      background-color: whitesmoke;
+      padding: 3px;
+      font-weight: bold;
+    }
+
+    ul{
+      margin-top: 10px;
+
+      li{
+        display: flex;
+
+        &:nth-child(n + 2){
+          margin-top: 10px;
+        }
+
+        p{
+          width: 50%;
+        }
+
+      }
+    }
+
+    div[data-type="shampoo"]{
+      width: 50%;
+
+    }
+
+    div[data-type="treatment"]{
+      width: 50%;
+      margin-left: 20px;
+
+    }
+  }
+
+}
 </style>
