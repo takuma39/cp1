e9f5f7e9797fc8550e8a539d5dfc4842ec25c06a
takuma39 <takuma.sato.job@gmail.com>
2022-09-12 18:34:39 +0900

    Fix HomeTop(SP)修正

--- src/components/HomeTop.vue
+++ src/components/HomeTop.vue
@@ -3,15 +3,17 @@
     <div class="homeTop__hero">
       <img alt="logo" src="@/assets/images/logo/logo.png">
     </div>
-    <div class="homeTop__circle" data-type="concept">
-      <router-link to="/concept">Concept</router-link>
-    </div>
-    <div class="homeTop__circle" data-type="products">
-      <router-link to="/products">Products</router-link>
-    </div>
-    <div class="homeTop__circle" data-type="shop">
-      <router-link to="/shop">Shop</router-link>
-    </div>
+    <ul class="homeTop__list">
+      <li class="homeTop__circle" data-type="concept">
+        <router-link to="/concept">Concept</router-link>
+      </li>
+      <li class="homeTop__circle" data-type="products">
+        <router-link to="/products">Products</router-link>
+      </li>
+      <li class="homeTop__circle" data-type="shop">
+        <router-link to="/shop">Shop</router-link>
+      </li>
+    </ul>
 
   </div>
 </template>
@@ -24,36 +26,66 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "@/assets/sass/app2.scss";
 .homeTop{
   position: relative;
   width: 100%;
-  height: calc(40vw + 200px);
-  min-height: 320px;
-  max-height: 750px;
+  display: flex;
+
+  // $break5: 600;
+  @include mq(u-br3) {
+    flex-direction: column;
+  }
 
   &__hero{
-    position: absolute;
-    left: 1vw;
-    top: 100px;
-    width: 40vw;
-    max-width: 550px;
-    z-index: 2;
+    width: 30%;
+    padding: 100px 0;
     animation: upDown 2s ease 0s infinite alternate none running;
+
+    // $break5: 600;
+    @include mq(u-br3) {
+      width: 50%;
+      padding: 0;
+      margin: 0 auto;
+    }
+  }
+
+  &__list{
+    position: relative;
+    height: auto;
+    width: 65%;
+    z-index: 100;
+
+    // $break5: 600;
+    @include mq(u-br3) {
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+      margin-top: 10px;
+    }
   }
 
   &__circle{
-    $circleMax:280px;
-    $circleMin:85px;
-    $length: 20vw;
-    height: $length;
-    min-height: $circleMin;
-    max-height: $circleMax;
-    width: $length;
-    min-width: $circleMin;
-    max-width: $circleMax;
+    $circleMin:150px;
+    $length: 30%;
+    padding-top: max($length, $circleMin);
+    width: max($length, $circleMin);
     background-color: rgba($color: #ffffff, $alpha: 0.8);
     border-radius: 50%;
     position: absolute;
+    transition: all 0.2s;
+
+    &:hover{
+      background-color: rgba($color: #63C8FF, $alpha: 0.8);
+      color: #fff;
+    }
+
+    // $break5: 600;
+    @include mq(u-br3) {
+      position: static;
+      padding-top: 25%;
+      width: 25%;
+    }
 
     a{
       display: flex;
@@ -62,33 +94,37 @@ export default {
       width: 100%;
       height: 100%;
       border-radius: 50%;
-      font-size: 20px;
+      @include rem(30);
       font-weight: bold;
-      transition: all 0.2s;
+      position: absolute;
+      top: 0;
 
-      &:hover{
-        background-color: rgba($color: #63C8FF, $alpha: 0.8);
-        color: #fff;
+      // $break5: 600;
+      @include mq(u-br3) {
+        @include rem(20);
+      }
+
+      // $break5: 480;
+      @include mq(u-br2) {
+        @include rem(15);
       }
     }
 
     &[data-type='concept']{
-      left: 53vw;
-      top: 10%;
+      left: 25%;
+      top: 5%;
       animation: upDown 2s ease 1.2s infinite alternate none running;
     }
 
     &[data-type='products']{
-      left: 73vw;
-      top: 350px;
-      top: 45%;
+      left:55%;
+      top: 40%;
       animation: upDown 2s ease 0.2s infinite alternate none running;
     }
 
     &[data-type='shop']{
-      left: 40vw;
-      top: 400px;
-      top: 60%;
+      left: 10%;
+      top: 55%;
       animation: upDown 2s ease 1.5s infinite alternate none running;
     }
   }
