eb12d6968736b207fc6c11fddcec5cb24ec820df
takuma39 <takuma.sato.job@gmail.com>
2022-09-14 15:30:29 +0900

    Fix ConceptView

--- src/views/concept/ConceptView.vue
+++ src/views/concept/ConceptView.vue
@@ -3,19 +3,22 @@
     <div class="concept__header">
       <PageTitle title="Concept"/>
     </div>
-    <div class="concept__bg">
-      <p>一人のためのXXX</p>
-    </div>
+    <ConceptBg/>
     <div class="concept__inner">
+      <ConceptText/>
     </div>
   </div>
 </template>
 
 
 <script>
+import ConceptBg from './components/ConceptBg.vue';
+import ConceptText from './components/ConceptText.vue';
 import PageTitle from '@/components/PageTitle.vue';
 export default {
   components: {
+    ConceptBg,
+    ConceptText,
     PageTitle
   },
 }
@@ -24,39 +27,9 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/sass/app2.scss";
 .concept{
-  padding: 100px 0 50px 0;
+  padding: 100px 0 0 0;
+  background-color: rgba($color: #fff, $alpha: 0.8);
 
-  &__bg{
-    width: 100%;
-    height: 500px;
-    margin-top: 30px;
-    background-image: url(@/assets/images/woman/woman2.jpg);
-    background-size: cover;
-    background-position: center;
-    position: relative;
-
-    &::after{
-      position: absolute;
-      content: "";
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      background-color: rgba($color: #fff, $alpha: 0.2);
-    }
-
-    p{
-      position: absolute;
-      content: "test";
-      bottom:50px;
-      left: 30px;
-      font-size: 100px;
-      font-weight: bold;
-      mix-blend-mode: overlay;
-      color: #fff;
-      z-index: 2;
-    }
-  }
 }
 
 
