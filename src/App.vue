e7760cd16d3d57b497c97797c58cddc675900645
takuma39 <takuma.sato.job@gmail.com>
2022-09-15 17:41:23 +0900

    Fix background修正

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
+  <div class="mobile-menu__cover"></div>
   <Header v-if="$route.path !== '/'" />
   <router-view/>
   <Footer/>
+  <Filter/>
 </template>
 
 <script>
+
 // @ is an alias to /src
 // import 'normalize.css';
 import Header from '@/layouts/HeaderLayout.vue';
 import Footer from '@/layouts/FooterLayout.vue';
+import Filter from '@/layouts/FilterLayout.vue';
 export default {
   name: 'AppView',
   components: {
     Header,
-    Footer
+    Footer,
+    Filter,
   },
   data(){
     return {
@@ -21,7 +26,6 @@ export default {
   },
   methods: {},
   mounted() {
-    console.log(this.$route.params);
   }
 }
 
@@ -34,8 +38,25 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: $black-1;
+  // position: relative;
+  // top:100px;
+  // left: -100px;
+  // transition: all 0.5s;
 }
 
-
-
+.mobile-menu__cover{
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  // width: 100%;
+  // height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  opacity: 0; 
+  visibility: hidden;
+  transition: opacity 1s;
+  cursor: pointer;
+  z-index: 1000000;
+}
 </style>
---
+++ src/layouts/FilterLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="filter"></div>
+</template>
+
+<script>
+export default {
+  name: 'FilterLayout',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/sass/app2.scss";
+#filter {
+  background-color: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(2px);
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: -1;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
