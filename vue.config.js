d914a9994a472d4e1eaa43e3999f4f8b587bcb2b
takuma39 <takuma.sato.job@gmail.com>
2022-09-16 13:14:08 +0900

    Add docs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: "/cp1", //リポジトリ名を指定
+  outputDir: "docs", //GitHubPagesで公開するにはdistではなくdocs配下にコンパイルする
+  transpileDependencies: true,
+});
